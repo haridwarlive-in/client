2143b70eef06bb7080cf9105544e48d1834cba30
Manik Sharma <maniksharma.rke@gmail.com>
2025-02-25 04:01:50 +0530

    files added

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ["haridwar-live.s3.ap-south-1.amazonaws.com", "images.unsplash.com"],
+    domains: ["haridwar-live.s3.ap-south-1.amazonaws.com", "images.unsplash.com", "haridwarlive-storage.s3.ap-south-1.amazonaws.com"],
   },
 };
 
